85303cb29c1c413e29bb56211499df7595d51979
Salisu Yahaya <salisu.yahaya@toptal.com>
2024-08-12 12:34:47 +0100

    Breakpoint for invoice ordered date

--- src/views/shop/Invoice.vue
+++ src/views/shop/Invoice.vue
@@ -113,7 +113,7 @@ async function downloadInvoice() {
             <div
               class="justify-end items-start px-6 pt-5 pb-6 text-base font-light tracking-normal leading-4 text-teal-500 bg-teal-50 rounded-md max-md:px-5 max-md:max-w-full md:mb-6"
             >
-              <div class="flex justify-between items-start">
+              <div class="flex flex-col gap-3 md:flex-row md:gap-0 justify-between items-start">
                 <div>
                   <h3 class="font-medium text-lg text-teal-500 mb-2">
                     Order placed, Thank you
@@ -123,7 +123,7 @@ async function downloadInvoice() {
                     Confirmation will be sent to your email.
                   </p>
                 </div>
-                <span class="text-theme-darkgray2"
+                <span class="text-theme-darkgray2 font-semibold"
                   >Ordered on
                   {{
                     new Date(order?.created_at)?.toLocaleDateString("en-GB", {
