0d0aab0e7b12a17a9c3ac8f2db477ceb94c39e08
Russell Keeley <russell.keeley@gmail.com>
2025-02-04 15:17:59 +0000

    chore: setup and configure eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import pluginVue from 'eslint-plugin-vue';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  ...pluginVue.configs['flat/essential'],
+  ...vueTsEslintConfig(),
+  {
+    files: ['src/**/__tests__/*'],
+  },
+  skipFormatting,
+  {
+    files: ['**/*.ts', '**/*.vue'],
+    plugins: { prettier },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'vue/multi-word-component-names': 'off',
+      'prettier/prettier': 'warn',
+      'vue/block-lang': [
+        'error',
+        {
+          script: { allowNoLang: true },
+        },
+      ],
+    },
+  },
+];
