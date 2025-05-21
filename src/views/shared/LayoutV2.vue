d972d30cf97ba954a48ee76818402c241a73fd40
Russell Keeley <russell.keeley@gmail.com>
2025-05-21 14:17:19 +0100

    restructure coupon components into separate folder

--- src/views/shared/LayoutV2.vue
+++ src/views/shared/LayoutV2.vue
@@ -3,14 +3,14 @@ import LayoutHeaderV2 from './HeaderV2.vue';
 import LayoutFooter from './Footer.vue';
 import Testimonials from './Testimonials.vue';
 import SubscribeForm from './SubscribeForm.vue';
-import WelcomeCoupon from '@/components/WelcomeCoupon.vue';
+import WelcomeCouponBanner from '@/components/coupons/WelcomeCouponBanner.vue';
 </script>
 
 <template>
   <div class="w-full max-w-full border-b border-theme-gray">
     <LayoutHeaderV2 class="max-w-8xl mx-auto"></LayoutHeaderV2>
   </div>
-  <WelcomeCoupon />
+  <WelcomeCouponBanner />
   <main class="w-full">
     <slot></slot>
   </main>
